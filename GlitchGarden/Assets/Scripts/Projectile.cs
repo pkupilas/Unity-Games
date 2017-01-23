20585b4f385a929c03291db8e864749d94df3502
pkupilas <pm.kupilas@gmail.com>
2017-01-23 14:57:59 +0100

    [GlitchGarden] Added animation controllers for projectiles. Modified animation controllers for defenders.

---
+++ GlitchGarden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.Translate(Vector3.right * speed * Time.deltaTime);
+	}
+}
