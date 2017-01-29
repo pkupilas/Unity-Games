3ae4bf47940e2deac914f12c349280ab7fceaf60
pkupilas <pm.kupilas@gmail.com>
2017-01-29 21:42:25 +0100

    [GlitchGarden] Added Tower Selector Buttons.

---
+++ GlitchGarden/Assets/Scripts/Button.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+    public GameObject defenderPrefab;
+
+    private Button[] buttons;
+    public static GameObject selectedDefender;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    buttons = FindObjectsOfType<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnMouseDown()
+    {
+        selectedDefender = defenderPrefab;
+        Debug.Log("selectedDefender: " + selectedDefender.name);
+        foreach (var _button in buttons)
+        {
+            _button.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+        gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+
+    }
+}
