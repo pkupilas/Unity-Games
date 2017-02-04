14ee27ac6a885ac93228cc8afa5e5e8c01bc9ffa
pkupilas <pm.kupilas@gmail.com>
2017-02-04 15:50:49 +0100

    [GlitchGarden] Added detecting attackers by defenders.

--- GlitchGarden/Assets/Scripts/Shooter.cs
+++ GlitchGarden/Assets/Scripts/Shooter.cs
@@ -7,17 +7,60 @@ public class Shooter : MonoBehaviour
     public GameObject gun;
 
     private GameObject projectileParent;
+    private Animator animator;
+    private Spawner myLaneSpawner;
 
     void Start()
     {
         projectileParent = GameObject.Find("Projectiles") ?? CreateProjectileParent();
+        animator = GetComponent<Animator>(); //FindObjectOfType<Animator>(); ?
+        SetMyLaneSpawner();
+    }
+    private void Update()
+    {
+        animator.SetBool("isAttacking", IsAttackerAheadInLane());
+    }
+
+    private void SetMyLaneSpawner()
+    {
+        Spawner[] spawnerArray = FindObjectsOfType<Spawner>();
+        var shooterPosition = transform.position;
+
+        if (spawnerArray.Length < 0)
+        {
+            Debug.LogError(name + ": There is no spawners.");
+            return;
+        }
+
+        foreach (Spawner spawner in spawnerArray)
+        {
+            if (spawner.transform.position.y == shooterPosition.y)
+            {
+                myLaneSpawner = spawner;
+                break;
+            }
+        }
+    }
+
+    private bool IsAttackerAheadInLane()
+    {
+        if (!myLaneSpawner || myLaneSpawner.transform.childCount <=0) return false;
+
+        foreach (Transform attacker in myLaneSpawner.transform)
+        {
+            if (attacker.position.x > transform.position.x)
+            {
+                return true;
+            }
+        }
+        
+        return false;
     }
 
     private GameObject CreateProjectileParent()
     {
         return new GameObject("Projectiles");
     }
-
 
     private void Fire()
     {
