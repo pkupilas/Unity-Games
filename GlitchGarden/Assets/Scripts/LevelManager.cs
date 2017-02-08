0a2cef50c82fc5493d8dfa923d2e34dedc3457a0
pkupilas <pm.kupilas@gmail.com>
2017-02-08 19:47:33 +0100

    [GlitchGarden] Added lose option.

--- GlitchGarden/Assets/Scripts/LevelManager.cs
+++ GlitchGarden/Assets/Scripts/LevelManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour
 {
-
     public float autoLoadTime;
 
     void Start()
---
+++ GlitchGarden/Assets/Scripts/LoseController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseController : MonoBehaviour
+{
+    public int loseCondition = 3;
+    public LevelManager levelManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    levelManager = FindObjectOfType<LevelManager>(); // posible nullptr exception
+	}
+	
+    private void OnTriggerEnter2D()
+    {
+        loseCondition--;
+        if (loseCondition <= 0)
+        {
+            levelManager.LoadLevel("03b Lose");
+        }
+    }
+}
