51aa8d045cfad4489fe7faae61002856082b7575
pkupilas <pm.kupilas@gmail.com>
2017-02-09 16:20:29 +0100

    [GlitchGarden] Added more levels, fixed their structure.

--- GlitchGarden/Assets/Scripts/Button.cs
+++ GlitchGarden/Assets/Scripts/Button.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Button : MonoBehaviour
 {
     public GameObject defenderPrefab;
+    public static GameObject selectedDefender;
 
     private Button[] buttons;
-    public static GameObject selectedDefender;
+    private Text costText;
 
 	// Use this for initialization
 	void Start ()
 	{
 	    buttons = FindObjectsOfType<Button>();
+	    costText = GetComponentInChildren<Text>();
+
+	    if (costText == null)
+	    {
+	        Debug.LogWarning(name + " has no cost text.");
+	    }
+	    else
+	    {
+	        costText.text = defenderPrefab.GetComponent<Defender>().starCost.ToString();
+	    }
 	}
 	
 	// Update is called once per frame
