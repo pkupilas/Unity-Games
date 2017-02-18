be6967da2d267bfd257e56da229b7f762229c151
pkupilas <pm.kupilas@gmail.com>
2017-02-18 15:02:51 +0100

    [Bowlmaster] Added IsStanding method. Removed Pin prefab.

---
+++ Bowlmaster/Bowlmaster/Assets/_Scripts/Pin.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour
+{
+
+    public float standingThreshold = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    Debug.Log(name + " " + IsStanding());
+	}
+
+    public bool IsStanding()
+    {
+        Vector3 rotationEulerAngles = transform.rotation.eulerAngles;
+
+        float rotXPositive = Mathf.Abs(rotationEulerAngles.x);
+        float tiltX = rotXPositive < 180f ? rotXPositive : 360 - rotXPositive;
+
+        float rotZPositive = Mathf.Abs(rotationEulerAngles.z);
+        float tiltZ = rotZPositive < 180f ? rotZPositive : 360 - rotZPositive;
+        
+        return standingThreshold > tiltX && standingThreshold > tiltZ;
+    }
+
+}
