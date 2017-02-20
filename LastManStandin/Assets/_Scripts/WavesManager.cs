bb4db40b7e40ce1e396492c03f96ae81d890ad2a
pkupilas <pm.kupilas@gmail.com>
2017-02-20 22:36:06 +0100

    [LastManStandin] Added slider to measure wave time.

---
+++ LastManStandin/Assets/_Scripts/WavesManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WavesManager : MonoBehaviour
+{
+    public float timeForWave = 60f;
+    public Slider slider;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateSlider();
+    }
+
+    private void UpdateSlider()
+    {
+        slider.value = Time.timeSinceLevelLoad / timeForWave;
+    }
+}
