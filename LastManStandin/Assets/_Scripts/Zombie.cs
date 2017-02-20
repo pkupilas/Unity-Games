3f41f0dfc40ab03f63c1e2b53fe280ee65d12dc8
pkupilas <pm.kupilas@gmail.com>
2017-02-20 13:21:49 +0100

    [LastManStandin] Added zombies. Updated prefabs.

---
+++ LastManStandin/Assets/_Scripts/Zombie.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour
+{
+
+    public float spawnRate = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ LastManStandin/Assets/_Scripts/ZombieSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieSpawner : MonoBehaviour
+{
+
+    public Zombie zombie;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (IsTimeToSpawn())
+	    {
+	        SpawnZombie();
+	    }
+	}
+
+    private bool IsTimeToSpawn()
+    {
+        float spawnPerSecond = 1 / zombie.spawnRate;
+
+        return Random.value < Time.deltaTime * spawnPerSecond;
+    }
+
+    private void SpawnZombie()
+    {
+        float x = Random.Range(-11.0f, 11.0f);
+        float y = Random.Range(-5.0f, 5.0f);
+
+        Zombie spawnedZombie = Instantiate(zombie);
+        spawnedZombie.transform.position = new Vector3(x, y, 0f);
+        spawnedZombie.transform.parent = FindObjectOfType<ZombieSpawner>().transform;
+
+    }
+}
