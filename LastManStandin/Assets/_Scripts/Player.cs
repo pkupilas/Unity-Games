bf2bc361ed3732b4f8e2cda2afbd66b8f7d0fbbe
pkupilas <pm.kupilas@gmail.com>
2017-02-20 10:49:10 +0100

    [LastManStandin] Added rotation according to mouse position.

--- LastManStandin/Assets/_Scripts/Player.cs
+++ LastManStandin/Assets/_Scripts/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
 	    HandleMovement();
+	    HandleRotation();
+
 	}
 
     private void HandleMovement()
@@ -34,6 +36,19 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             _transform.position += new Vector3(0, -0.1f, 0);
+        }
+    }
+
+    private void HandleRotation()
+    {
+        //TODO: Improve messy rotation code
+        //Debug.Log(_transform.rotation.eulerAngles);
+        var mousePosition = Input.mousePosition;
+        var mouseRotation = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, mousePosition.z - _transform.position.z));
+
+        if ((_transform.position.x != mousePosition.y) && (_transform.position.y != mousePosition.y))
+        {
+            _transform.rotation = Quaternion.Slerp(_transform.rotation, Quaternion.Euler(0, 0, Mathf.Atan2((mouseRotation.y - _transform.position.y), (mouseRotation.x - _transform.position.x)) * Mathf.Rad2Deg - 90), 2.0f * Time.deltaTime);
         }
     }
 
