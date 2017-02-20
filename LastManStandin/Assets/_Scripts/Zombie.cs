19505eb1d839c052112c3ac63d0e43651c2f1425
pkupilas <pm.kupilas@gmail.com>
2017-02-20 14:36:40 +0100

    [LastManStandin] Added base, zombies may change their target. Added UI element for base health.

--- LastManStandin/Assets/_Scripts/Zombie.cs
+++ LastManStandin/Assets/_Scripts/Zombie.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.InteropServices;
 
 public class Zombie : MonoBehaviour
 {
 
     public float spawnRate = 5;
     public float speed = 2;
-
+    private Vector3 objectOfInterestPosition;
+    private Base _base;
     private Player _player;
+
     // Use this for initialization
 	void Start ()
 	{
 	    _player = FindObjectOfType<Player>();
+	    _base = FindObjectOfType<Base>();
 	}
 	
 	// Update is called once per frame
@@ -27,11 +31,37 @@ public class Zombie : MonoBehaviour
             Destroy(coll.gameObject);
             Destroy(gameObject);
         }
+        else if (coll.gameObject.GetComponent<Base>() != null)
+        {
+            StopMoving();
+        }
+    }
+
+    private void StopMoving()
+    {
+        speed = 0;
     }
 
     private void HandleMoving()
     {
         float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, step);
+
+        UpdateObjectOfInterest();
+        transform.position = Vector3.MoveTowards(transform.position, objectOfInterestPosition, step);
+    }
+
+    private void UpdateObjectOfInterest()
+    {
+        float distanceToPlayer = GetVectorLength(_player.transform);
+        float distanceToBase = GetVectorLength(_base.transform);
+
+        objectOfInterestPosition = distanceToPlayer > distanceToBase ? _base.transform.position : _player.transform.position;
+    }
+
+    private float GetVectorLength(Transform objTransform)
+    {
+        Vector3 length = objTransform.position - transform.position;
+
+        return length.magnitude;
     }
 }
