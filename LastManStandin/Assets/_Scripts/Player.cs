8adae70d87b0c81d5b92e3aa3009757ce83f453e
pkupilas <pm.kupilas@gmail.com>
2017-02-20 10:20:58 +0100

    [LastManStandin] Added player and simple movement, background.

---
+++ LastManStandin/Assets/_Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    private Transform _transform;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _transform = GetComponent<Transform>(); // possible nullptr 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    HandleMovement();
+	}
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            _transform.position += new Vector3(-0.1f,0,0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _transform.position += new Vector3(0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            _transform.position += new Vector3(0, 0.1f, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _transform.position += new Vector3(0, -0.1f, 0);
+        }
+    }
+
+
+}
