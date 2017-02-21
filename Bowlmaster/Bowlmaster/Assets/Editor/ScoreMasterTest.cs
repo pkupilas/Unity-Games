51ed7c28ff0561a52db06c825b4d8037eb74362a
pkupilas <pm.kupilas@gmail.com>
2017-02-21 14:32:59 +0100

    [Bowlmaster] Introduced TDD with Unity Test Tools.

---
+++ Bowlmaster/Bowlmaster/Assets/Editor/ScoreMasterTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class ScoreMasterTest {
+
+    [Test]
+    public void FailingTest()
+    {
+        Assert.AreEqual(1,1);
+    }
+}
