0ad9dbbe146ad3257320dbe910b3958930e559bd
pkupilas <pm.kupilas@gmail.com>
2017-02-21 15:50:00 +0100

    [Bowlmaster] Added T02Bowl8ReturnsTidy, T03After21BowlsReturnsEndGame, T04Bowl2And8SpareReturnsEndTurn tests.

--- Bowlmaster/Bowlmaster/Assets/Editor/ActionMasterTest.cs
+++ Bowlmaster/Bowlmaster/Assets/Editor/ActionMasterTest.cs
@@ -5,7 +5,16 @@ using NUnit.Framework;
 public class ActionMasterTest
 {
 
-    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action _endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action _tidy = ActionMaster.Action.Tidy;
+    private ActionMaster.Action _endGame = ActionMaster.Action.EndGame;
+    private ActionMaster _actionMaster;
+
+    [SetUp]
+    public void Setup()
+    {
+         _actionMaster = new ActionMaster();
+    }
 
     [Test]
     public void T00PassingTest()
@@ -16,7 +25,31 @@ public class ActionMasterTest
     [Test]
     public void T01OneStrikeReturnsEndTurn()
     {
-        var actionMaster = new ActionMaster();
-        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+        Assert.AreEqual(_endTurn, _actionMaster.Bowl(10));
     }
+
+    [Test]
+    public void T02Bowl8ReturnsTidy()
+    {
+        Assert.AreEqual(_tidy, _actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void T03After21BowlsReturnsEndGame()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            _actionMaster.Bowl(1);
+        }
+
+        Assert.AreEqual(_endGame, _actionMaster.Bowl(1));
+    }
+
+    [Test]
+    public void T04Bowl2And8SpareReturnsEndTurn()
+    {
+        _actionMaster.Bowl(2);
+        Assert.AreEqual(_endTurn,_actionMaster.Bowl(8));
+    }
+
 }
--- Bowlmaster/Bowlmaster/Assets/_Scripts/ActionMaster.cs
+++ Bowlmaster/Bowlmaster/Assets/_Scripts/ActionMaster.cs
@@ -5,8 +5,15 @@ public class ActionMaster {
 
     public enum Action {Tidy, Reset, EndTurn, EndGame}
 
+    private int bowlNumber = 1;
+
     public Action Bowl(int pins)
     {
+        if (bowlNumber == 21)
+        {
+            return Action.EndGame;
+        }
+
         if (pins < 0 || pins>10)
         {
             throw new UnityException("Pin count cannot be less than 0 and bigger than 10");
@@ -14,8 +21,22 @@ public class ActionMaster {
 
         if (pins == 10)
         {
+            bowlNumber += 2;
             return Action.EndTurn;
         }
+
+        if (bowlNumber % 2 != 0)
+        {
+            bowlNumber++;
+            return Action.Tidy;
+        }
+
+        if (bowlNumber % 2 == 0)
+        {
+            bowlNumber++;
+            return Action.EndTurn;
+        }
+
         throw new UnityException("Not sure what action to return.");
     }
 }
