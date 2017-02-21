6bb1f1e89438a37c44979e8a5405d98695d0e274
pkupilas <pm.kupilas@gmail.com>
2017-02-21 15:09:36 +0100

    [Bowlmaster] Added T01OneStrikeReturnsEndTurn test.

---
+++ Bowlmaster/Bowlmaster/Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class ActionMasterTest
+{
+
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
+    [Test]
+    public void T00PassingTest()
+    {
+        Assert.AreEqual(1,1);
+    }
+
+    [Test]
+    public void T01OneStrikeReturnsEndTurn()
+    {
+        var actionMaster = new ActionMaster();
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+}
---
+++ Bowlmaster/Bowlmaster/Assets/_Scripts/ActionMaster.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionMaster {
+
+    public enum Action {Tidy, Reset, EndTurn, EndGame}
+
+    public Action Bowl(int pins)
+    {
+        if (pins < 0 || pins>10)
+        {
+            throw new UnityException("Pin count cannot be less than 0 and bigger than 10");
+        }
+
+        if (pins == 10)
+        {
+            return Action.EndTurn;
+        }
+        throw new UnityException("Not sure what action to return.");
+    }
+}
