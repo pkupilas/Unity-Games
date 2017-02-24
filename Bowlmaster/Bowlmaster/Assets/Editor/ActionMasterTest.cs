bbf4b8727feaec2f1b04f6a093fb7bda3a553ed3
pkupilas <pm.kupilas@gmail.com>
2017-02-24 20:45:58 +0100

    [Bowlmaster] Commented ActionMasterTests. Added ScoreMasterTests and started implementation of ScoreFrame.

--- Bowlmaster/Bowlmaster/Assets/Editor/ActionMasterTest.cs
+++ Bowlmaster/Bowlmaster/Assets/Editor/ActionMasterTest.cs
@@ -7,87 +7,87 @@ using System.Linq;
 public class ActionMasterTest
 {
 
-    private ActionMaster.Action _endTurn = ActionMaster.Action.EndTurn;
-    private ActionMaster.Action _tidy = ActionMaster.Action.Tidy;
-    private ActionMaster.Action _endGame = ActionMaster.Action.EndGame;
-    private ActionMaster.Action _reset = ActionMaster.Action.Reset;
+    //private ActionMaster.Action _endTurn = ActionMaster.Action.EndTurn;
+    //private ActionMaster.Action _tidy = ActionMaster.Action.Tidy;
+    //private ActionMaster.Action _endGame = ActionMaster.Action.EndGame;
+    //private ActionMaster.Action _reset = ActionMaster.Action.Reset;
 
-    [Test]
-    public void T01OneStrikeReturnsEndTurn()
-    {
-        int[] rolls = {10};
-        Assert.AreEqual(_endTurn, ActionMaster.NextAction(rolls.ToList()));
-    }
+    //[Test]
+    //public void T01OneStrikeReturnsEndTurn()
+    //{
+    //    int[] rolls = {10};
+    //    Assert.AreEqual(_endTurn, ActionMaster.NextAction(rolls.ToList()));
+    //}
 
-    [Test]
-    public void T02Bowl8ReturnsTidy()
-    {
-        int[] rolls = { 8 };
-        Assert.AreEqual(_tidy, ActionMaster.NextAction(rolls.ToList()));
-    }
+    //[Test]
+    //public void T02Bowl8ReturnsTidy()
+    //{
+    //    int[] rolls = { 8 };
+    //    Assert.AreEqual(_tidy, ActionMaster.NextAction(rolls.ToList()));
+    //}
 
-    [Test]
-    public void T03After20BowlsReturnsEndGame()
-    {
-        int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-        Assert.AreEqual(_endGame, ActionMaster.NextAction(rolls.ToList()));
-    }
+    //[Test]
+    //public void T03After20BowlsReturnsEndGame()
+    //{
+    //    int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    //    Assert.AreEqual(_endGame, ActionMaster.NextAction(rolls.ToList()));
+    //}
 
-    [Test]
-    public void T04Bowl2And8SpareReturnsEndTurn()
-    {
-        int[] rolls = {2, 8};
-        Assert.AreEqual(_endTurn, ActionMaster.NextAction(rolls.ToList()));
-    }
+    //[Test]
+    //public void T04Bowl2And8SpareReturnsEndTurn()
+    //{
+    //    int[] rolls = {2, 8};
+    //    Assert.AreEqual(_endTurn, ActionMaster.NextAction(rolls.ToList()));
+    //}
 
-    [Test]
-    public void T05AtStrikeInLastFrameReturnsReset()
-    {
-        int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10};
-        Assert.AreEqual(_reset, ActionMaster.NextAction(rolls.ToList()));
-    }
+    //[Test]
+    //public void T05AtStrikeInLastFrameReturnsReset()
+    //{
+    //    int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10};
+    //    Assert.AreEqual(_reset, ActionMaster.NextAction(rolls.ToList()));
+    //}
 
-    [Test]
-    public void T06AtSpareInLastFrameReturnsReset()
-    {
-        int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9};
-        Assert.AreEqual(_reset, ActionMaster.NextAction(rolls.ToList()));
-    }
+    //[Test]
+    //public void T06AtSpareInLastFrameReturnsReset()
+    //{
+    //    int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9};
+    //    Assert.AreEqual(_reset, ActionMaster.NextAction(rolls.ToList()));
+    //}
 
-    [Test]
-    public void T07YouTubeRollsReturnsEndGame()
-    {
-        int[] rolls = {8, 2, 7, 3, 3, 4, 10, 2, 8, 10, 10, 8, 0, 10, 8, 2, 9};
-        Assert.AreEqual(_endGame, ActionMaster.NextAction(rolls.ToList()));
-    }
+    //[Test]
+    //public void T07YouTubeRollsReturnsEndGame()
+    //{
+    //    int[] rolls = {8, 2, 7, 3, 3, 4, 10, 2, 8, 10, 10, 8, 0, 10, 8, 2, 9};
+    //    Assert.AreEqual(_endGame, ActionMaster.NextAction(rolls.ToList()));
+    //}
 
-    [Test]
-    public void T08NotKnockAllPinsAfterStrikeOnBowl19ReturnsTidy()
-    {
-        int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 9};
-        Assert.AreEqual(_tidy, ActionMaster.NextAction(rolls.ToList()));
-    }
+    //[Test]
+    //public void T08NotKnockAllPinsAfterStrikeOnBowl19ReturnsTidy()
+    //{
+    //    int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 9};
+    //    Assert.AreEqual(_tidy, ActionMaster.NextAction(rolls.ToList()));
+    //}
 
-    [Test]
-    public void T09StrikeIn19AndZeroIn20ReturnsTidy()
-    {
-        int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 0};
-        Assert.AreEqual(_tidy, ActionMaster.NextAction(rolls.ToList()));
-    }
+    //[Test]
+    //public void T09StrikeIn19AndZeroIn20ReturnsTidy()
+    //{
+    //    int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 0};
+    //    Assert.AreEqual(_tidy, ActionMaster.NextAction(rolls.ToList()));
+    //}
 
-    [Test]
-    public void T10NathanBowlIndexTest()
-    {
-        // Bowl 0 pins and after that 10 pins.
-        // That should not increment bowlNumber twice
-        int[] rolls = { 0, 10, 5, 1 };
-        Assert.AreEqual(_endTurn, ActionMaster.NextAction(rolls.ToList()));
-    }
+    //[Test]
+    //public void T10NathanBowlIndexTest()
+    //{
+    //    // Bowl 0 pins and after that 10 pins.
+    //    // That should not increment bowlNumber twice
+    //    int[] rolls = { 0, 10, 5, 1 };
+    //    Assert.AreEqual(_endTurn, ActionMaster.NextAction(rolls.ToList()));
+    //}
 
-    [Test]
-    public void T11Dondi10thFrameTurkey()
-    {
-        int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 10, 10};
-        Assert.AreEqual(_endGame, ActionMaster.NextAction(rolls.ToList()));
-    }
+    //[Test]
+    //public void T11Dondi10thFrameTurkey()
+    //{
+    //    int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 10, 10};
+    //    Assert.AreEqual(_endGame, ActionMaster.NextAction(rolls.ToList()));
+    //}
 }
--- Bowlmaster/Bowlmaster/Assets/_Scripts/ScoreMaster.cs
+++ Bowlmaster/Bowlmaster/Assets/_Scripts/ScoreMaster.cs
@@ -4,9 +4,49 @@ using System.Collections.Generic;
 
 public class ScoreMaster {
 
-    public List<int> ScoreFrames(List<int> scoreFrames)
+    private const int _STRIKE = 10;
+
+    public static List<int> ScoreCumulative(List<int> rolls)
+    {
+        var cumulativeScoresList = new List<int>();
+        bool lastRollWasStrike = false;
+        var scoreFrames = ScoreFrames(rolls);
+        int sum = 0;
+
+        foreach (var score in scoreFrames)
+        {
+            sum += score;
+            cumulativeScoresList.Add(sum);
+        }
+
+        return cumulativeScoresList;
+    }
+
+
+    public static List<int> ScoreFrames(List<int> rolls)
     {
         var frameList = new List<int>();
+        int firstScoreFrame = 0;
+        int secondScoreFrame = 0;
+
+        foreach (var roll in rolls)
+        {
+            if (roll == _STRIKE)
+            {
+                frameList.Add(_STRIKE);
+            }
+            else if(firstScoreFrame==0)
+            {
+                firstScoreFrame = roll;
+            }
+            else if(secondScoreFrame==0)
+            {
+                secondScoreFrame = roll;
+                frameList.Add(firstScoreFrame+secondScoreFrame);
+                firstScoreFrame = 0;
+                secondScoreFrame = 0;
+            }
+        }
 
         return frameList;
     }
