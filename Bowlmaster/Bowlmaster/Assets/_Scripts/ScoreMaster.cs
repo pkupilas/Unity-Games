8b312a7b8044bf50dbf6894189b64e5fb4fa0ee2
pkupilas <pm.kupilas@gmail.com>
2017-02-25 17:35:49 +0100

    [Bowlmaster] Fixed ScoreFrames. Made verification tests working.

--- Bowlmaster/Bowlmaster/Assets/_Scripts/ScoreMaster.cs
+++ Bowlmaster/Bowlmaster/Assets/_Scripts/ScoreMaster.cs
@@ -26,25 +26,23 @@ public class ScoreMaster {
 
         for (int i = 1; i < rolls.Count; i+=2)
         {
-            if (frames.Count == 10) break;
-
-            //NORMAL FRAME
-            if (rolls[i] + rolls[i - 1] < 10)
+            if (frames.Count == 10) break;          // PREVENT 11 FRAME
+            
+            if (rolls[i] + rolls[i - 1] < 10)       // NORMAL FRAME
             {
                 frames.Add(rolls[i] + rolls[i - 1]);
             }
 
-            if (i + 1 == rolls.Count) break;
+            if (i + 1 >= rolls.Count)  break;       // ENSURE NEXT ELEMENT EXISTS
 
-            //SPARE BONUS
-            if (rolls[i] + rolls[i - 1] == 10)
+            if (rolls[i - 1] == 10)                 // STRIKE BONUS
+            {
+                i--;                                // modify due to strike bonus (i+2)
+                frames.Add(10 + rolls[i + 1] + rolls[i + 2]);
+            }
+            else if (rolls[i] + rolls[i - 1] == 10) // SPARE BONUS 
             {
                 frames.Add(10+rolls[i+1]);
-            }
-            else if (rolls[i - 1] == 10) // STRIKE BONUS
-            {
-                i--;                     // modify i due to strike bonus (i+2)
-                frames.Add(10 + rolls[i + 1] + rolls[i + 2]);
             }
         }
          
