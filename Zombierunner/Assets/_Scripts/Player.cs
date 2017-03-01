baf219de589925e209f608a7d62acb7049338d81
pkupilas <pm.kupilas@gmail.com>
2017-03-01 14:09:12 +0100

    [Zombierunner] Added spawnpoints and player respawning.

---
+++ Zombierunner/Assets/_Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    public bool reSpawner = false;
+
+    private Transform[] _spawnPoints;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _spawnPoints = GameObject.Find("Player Spawn Points").GetComponentsInChildren<Transform>();
+	    ReSpawn();
+	}
+
+    // Update is called once per frame
+	void Update () {
+	    if (reSpawner)
+	    {
+	        ReSpawn();
+	        reSpawner = false;
+	    }
+	}
+
+    private void ReSpawn()
+    {
+        int randomIndex = Random.Range(1, _spawnPoints.Length);
+        transform.position = _spawnPoints[randomIndex].position;
+    }
+
+}
