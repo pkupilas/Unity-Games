d65584800d57a0f98c1c2ecf25ce51e5741f770e
pkupilas <pm.kupilas@gmail.com>
2017-03-02 12:25:31 +0100

    [Zombierunner] Created day-night cycle.

---
+++ Zombierunner/Assets/_Scripts/Daycycle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Daycycle : MonoBehaviour
+{
+    [Tooltip("Number of minutes per second. ")]
+    public float timeScale = 60f;
+    
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        RotateSun();
+	}
+
+    private void RotateSun()
+    {
+        var anglePerFrame = Time.deltaTime / 360f * timeScale;
+        transform.RotateAround(transform.position,Vector3.forward,anglePerFrame);
+    }
+}
