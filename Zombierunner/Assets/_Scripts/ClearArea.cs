d2111799376cdf9d1c205330ea95f19fae96bfed
pkupilas <pm.kupilas@gmail.com>
2017-03-02 13:13:01 +0100

    [Zombierunner] Added CleanArea.cs

---
+++ Zombierunner/Assets/_Scripts/ClearArea.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearArea : MonoBehaviour
+{
+
+    private float _timeSinceLastTrigger = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    _timeSinceLastTrigger += Time.deltaTime;
+	    if (_timeSinceLastTrigger > 1f)
+	    {
+	        Debug.Log("Clear area!");
+	    }
+	}
+
+    private void OnTriggerStay(Collider coll)
+    {
+        _timeSinceLastTrigger = 0f;
+    }
+}
