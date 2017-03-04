22b1b6648456ce27ff17c879a32f571f5652a5d2
pkupilas <pm.kupilas@gmail.com>
2017-03-04 17:33:26 +0100

    [TwinStick] Added assets pack and turned on second controller trigger.

---
+++ TwinStick/Assets/_Scripts/CameraPan.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CameraPan : MonoBehaviour
+{
+
+    private GameObject _player;
+
+	// Use this for initialization
+	void Start () {
+	    _player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+
+	void LateUpdate ()
+    {
+        transform.LookAt(_player.transform);
+    }
+}
---
+++ TwinStick/Assets/_Scripts/SelfieStick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SelfieStick : MonoBehaviour
+{
+
+    public float panSpeed = 2f;
+
+    private GameObject _player;
+    private Vector3 armRotation;
+
+
+    // Use this for initialization
+    void Start () {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        armRotation = transform.rotation.eulerAngles;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    armRotation.y += CrossPlatformInputManager.GetAxis("RHorizontal")*panSpeed;
+	    armRotation.x += CrossPlatformInputManager.GetAxis("RVertical")* panSpeed;
+	    transform.position = _player.transform.position;
+        transform.rotation = Quaternion.Euler(armRotation);
+
+	}
+}
