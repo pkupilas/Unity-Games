8b3aff9cd4867fd57e0a53915310d536f30c8873
pkupilas <pm.kupilas@gmail.com>
2017-03-04 16:17:22 +0100

    [TwinStick] Added pausing.

--- TwinStick/Assets/_Scripts/GameManager.cs
+++ TwinStick/Assets/_Scripts/GameManager.cs
@@ -8,17 +8,36 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     public bool Recording { get; private set; }
 
+    private float initialFixedDeltaTime;
+
 	// Use this for initialization
 	void Start ()
 	{
 	    Recording = true;
+	    initialFixedDeltaTime = Time.fixedDeltaTime;
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 	    ManagePlayback();
+	    CheckIfPause();
 	}
+
+    private void CheckIfPause()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("PauseButton"))
+        {
+            Time.timeScale = 0f;
+            Time.fixedDeltaTime = 0f;
+        }
+        if (CrossPlatformInputManager.GetButtonUp("PauseButton"))
+        {
+            Time.timeScale = 1f;
+            Time.fixedDeltaTime = initialFixedDeltaTime;
+        }
+    }
 
     void ManagePlayback()
     {
