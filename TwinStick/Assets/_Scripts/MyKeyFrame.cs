7ba8d8761c16879f7e1a186b95d5bb2055e7c7f8
pkupilas <pm.kupilas@gmail.com>
2017-03-04 12:35:49 +0100

    [TwinStick] Added MyKeyFrame.cs and MyReplay.cs.

---
+++ TwinStick/Assets/_Scripts/MyKeyFrame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyKeyFrame
+{
+
+    public float Time { get; private set; }
+    public Vector3 Pos { get; private set; }
+    public Quaternion Rot { get; private set; }
+
+    public MyKeyFrame(float time, Vector3 pos, Quaternion rot)
+    {
+        Time = time;
+        Pos = pos;
+        Rot = rot;
+    }
+}
---
+++ TwinStick/Assets/_Scripts/MyReplay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyReplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
