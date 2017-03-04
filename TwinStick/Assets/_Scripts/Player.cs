e9bd1164f2d6ba5d9ea8f8b66f50d27153f37007
pkupilas <pm.kupilas@gmail.com>
2017-03-04 11:57:36 +0100

    [TwinStick] Added CrossPlatformInput assets.

---
+++ TwinStick/Assets/_Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+}
