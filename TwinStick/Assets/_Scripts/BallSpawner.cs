86d23eb5cc5dac1ae1aa3df54834a9f467e4f97c
pkupilas <pm.kupilas@gmail.com>
2017-03-06 19:33:01 +0100

    [TwinStick] #19 Added menu scene, level manager.

---
+++ TwinStick/Assets/_Scripts/BallSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour
+{
+
+    public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+        SpawnBall();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    //float threshold = Time.deltaTime;
+     //   Debug.Log(threshold);
+     //   if (Random.value < threshold)
+     //   {
+     //       SpawnBall();
+
+     //   }
+	}
+
+    private void SpawnBall()
+    {
+        var ballObject = Instantiate(ball);
+        ballObject.transform.position = transform.position;
+        //ballObject.GetComponent<Rigidbody>().velocity = new Vector3(100,0,0);
+    }
+
+}
---
+++ TwinStick/Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    private static int levelIndex = 0;
+	// Use this for initialization
+	void Start () {
+	    DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadNextLevel()
+    {
+        levelIndex++;
+        SceneManager.LoadScene(levelIndex);
+    }
+}
