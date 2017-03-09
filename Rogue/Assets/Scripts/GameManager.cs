b16efc5c1ac2ee84c8c03280d57b3f56f9111808
pkupilas <pm.kupilas@gmail.com>
2017-03-09 16:20:16 +0100

    [Rogue] Set up GameManager.

--- Rogue/Assets/Scripts/GameManager.cs
+++ Rogue/Assets/Scripts/GameManager.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+
+    public BoardManager boardScript;
+
+    private int level = 3;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake ()
+	{
+	    boardScript = GetComponent<BoardManager>();
+	    InitGame();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private void InitGame()
+    {
+
+        boardScript.SetupScene(level);
+    }
+
 }
