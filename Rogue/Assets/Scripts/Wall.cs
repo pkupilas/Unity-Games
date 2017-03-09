c0b3b9e630eaa571f4fb0d165c207b2ed1f20e54
pkupilas <pm.kupilas@gmail.com>
2017-03-09 17:43:24 +0100

    [Rogue] Added Wall.cs.

---
+++ Rogue/Assets/Scripts/Wall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour
+{
+
+    public Sprite dmgSprite;
+    public int hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+
+
+	// Use this for initialization
+	void Awake ()
+	{
+	    spriteRenderer = GetComponent<SpriteRenderer>();
+
+	}
+
+    public void DamageWall(int loss)
+    {
+        spriteRenderer.sprite = dmgSprite;
+        hp -= loss;
+        if (hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
