c48aa303935fd9936267ea823682d837360e4997
pkupilas <pm.kupilas@gmail.com>
2017-03-11 20:02:35 +0100

    [AhoyMatey] Added MyNetworkManager.cs, removed MyHud.cs.

---
+++ AhoyMatey/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+    
+    public void MyStartHost()
+    {
+        Debug.Log("Starting host at: " + Time.timeSinceLevelLoad);
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log("Host started at: " + Time.timeSinceLevelLoad);
+    }
+
+}
