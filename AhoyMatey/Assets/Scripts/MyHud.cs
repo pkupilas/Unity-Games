8f66f25af84bfd205be89c95a2abaeaf46049832
pkupilas <pm.kupilas@gmail.com>
2017-03-11 19:43:56 +0100

    [AhoyMatey] Replaced NetworkManagerHUD.

---
+++ AhoyMatey/Assets/Scripts/MyHud.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyHud : MonoBehaviour
+{
+
+    private NetworkManager _networkManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _networkManager = GetComponent<NetworkManager>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MyStartHost()
+    {
+        Debug.Log("Starting host at: " + Time.timeSinceLevelLoad);
+        _networkManager.StartHost();
+    }
+
+    void OnStartHost()
+    {
+        Debug.Log("Starting host at: " + Time.timeSinceLevelLoad);
+    }
+
+}
