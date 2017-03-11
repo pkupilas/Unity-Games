4de0f0704957c41d1bb5aad8feb7339ea803a5ea
pkupilas <pm.kupilas@gmail.com>
2017-03-11 21:50:00 +0100

    [AhoyMatey] Added CrossPlatformInput assets, players movement with local authority..

---
+++ AhoyMatey/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : NetworkBehaviour
+{
+
+    private Vector3 _inputValue;
+    
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    if (!isLocalPlayer)
+	    {
+	        return;
+	    }
+
+	    _inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+	    _inputValue.y = 0f;
+        _inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+        transform.Translate(_inputValue);
+    }
+}
