69f3720bc6a368c65acf635ae3c875b09f8b7780
pkupilas <pm.kupilas@gmail.com>
2017-03-22 20:31:39 +0100

    [DragonRPG] Added follow camera.

---
+++ DragonRPG/Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private GameObject _player;
+
+
+	// Use this for initialization
+	void Start () {
+	    _player = GameObject.FindWithTag("Player");	
+	}
+	
+	void LateUpdate ()
+	{
+	    transform.position = _player.transform.position;
+    }
+
+
+}
