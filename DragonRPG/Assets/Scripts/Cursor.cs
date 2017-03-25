e6c8a1dc9ee78a1624a7c9b2f47c30cda02997d7
pkupilas <pm.kupilas@gmail.com>
2017-03-25 21:43:10 +0100

    [DragonRPG] Deleted standard cameras. Added raycasts to query mouse position.

---
+++ DragonRPG/Assets/Scripts/Cursor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+
+    private CameraRaycaster _cameraRaycaster;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _cameraRaycaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Debug.Log(_cameraRaycaster.layerHit);
+	}
+}
---
+++ DragonRPG/Assets/Scripts/Utility.cs
@@ -0,0 +1,6 @@
+﻿public enum Layer
+{
+    Walkable = 8,
+    Enemy = 9,
+    RaycastEndStop = -1
+}
