16fb598b46ea02e2caa1f211255e9403b7120b66
pkupilas <pm.kupilas@gmail.com>
2017-06-10 13:11:26 +0200

    [Canteen] Added arrow mechanism.

---
+++ CanteenWars/Assets/Scripts/Arrow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private Animator _animator;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+    
+    public void StopRotating()
+    {
+        var arrowRotation = transform.rotation.eulerAngles;
+        _animator.SetTrigger("CurveArrow");
+        transform.eulerAngles = arrowRotation;
+    }
+
+    public void StopCurve()
+    {
+        var arrowScale = transform.localScale;
+        transform.localScale = arrowScale;
+        _animator.Stop();
+    }
+}
---
+++ CanteenWars/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private Arrow _arrow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        _arrow.StopRotating();
+	    }
+        else if (Input.GetKeyUp(KeyCode.Space))
+	    {
+	        _arrow.StopCurve();
+	    }
+
+    }
+}
