4a5d930df63e5b7bcce1c67ba40e1b921ff68fc0
pkupilas <pm.kupilas@gmail.com>
2017-07-06 16:44:24 +0200

    [DragonRPG] Added healthbar.

---
+++ DragonRPG/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float _maxPlayerHealth = 100f;
+    private float _currentPlayerHealth = 100f;
+
+    public float HealthAsPercentage
+    {
+        get { return _currentPlayerHealth / _maxPlayerHealth; }
+    }
+}
