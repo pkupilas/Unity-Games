2ebf5cac4a7af2858f11d6b67fdcc089e1ee4fae
pkupilas <pm.kupilas@gmail.com>
2017-07-07 19:13:24 +0200

    [DragonRPG] Added enemy healthbar.

---
+++ DragonRPG/Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField]
+    private float _maxEnemyHealth = 100f;
+    private float _currentEnemyHealth = 100f;
+
+    public float HealthAsPercentage
+    {
+        get { return _currentEnemyHealth / _maxEnemyHealth; }
+    }
+}
