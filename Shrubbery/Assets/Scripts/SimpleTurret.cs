fbd917a594593270871c7126d9b9cf120d8e2762
pkupilas <pm.kupilas@gmail.com>
2017-07-20 11:51:50 +0200

    [Shrubbery] Added targeting enemy.

--- Shrubbery/Assets/Scripts/SimpleTurret.cs
+++ Shrubbery/Assets/Scripts/SimpleTurret.cs
@@ -1,12 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class SimpleTurret : MonoBehaviour
 {
     [SerializeField] private GameObject _ammunition;
     [SerializeField] private GameObject _spawnPoint;
+    [SerializeField] private float _range = 2f;
+    private GameObject _target;
 
-	void Update ()
+
+    void Update ()
     {
+        CheckForTargets();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Shoot();
@@ -16,5 +21,23 @@ public class SimpleTurret : MonoBehaviour
     private void Shoot()
     {
         GameObject projectile = Instantiate(_ammunition,_spawnPoint.transform);
+    }
+
+    void OnDrawGizmos()
+    {
+        Color color = Color.yellow;
+        color.a = 0.5f;
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere(transform.position,_range);
+    }
+
+    void CheckForTargets()
+    {
+        var colliderList = Physics.OverlapSphere(transform.position, _range, 1 << LayerMask.NameToLayer("Enemy")).ToList();
+
+        if (_target==null || !colliderList.Contains(_target.GetComponent<Collider>()))
+        {
+            _target = colliderList.Count > 0 ? colliderList[0].gameObject : null;
+        }
     }
 }
