8743bc3c3fb123c04c5f83ce203e5a69b7a787e5
Piotr <pm.kupilas@gmail.com>
2017-07-20 16:36:39 +0200

    [Shrubbery] Fixes. (#27)
    Squashed changes with towers shooting.

--- Shrubbery/Assets/Scripts/Projectile.cs
+++ Shrubbery/Assets/Scripts/Projectile.cs
@@ -3,23 +3,28 @@
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float _velocity = 4f;
-    [SerializeField] private float _damage = 33f;
+    [SerializeField] private float _damage = 1f;
+
+    public float Damage
+    {
+        get { return _damage; }
+        set { _damage = value; }
+    }
+    public float Velocity
+    {
+        get { return _velocity; }
+        set { _velocity = value; }
+    }
 
     private Rigidbody _rigidbody;
     
-	void Start ()
-	{
-	    _rigidbody = GetComponent<Rigidbody>();
-	    _rigidbody.velocity = new Vector3(-1 * _velocity, 0, 0);
-	}
-	
     void OnTriggerEnter(Collider other)
     {
         var component = other.gameObject.GetComponent(typeof(IDamageable));
         if (component)
         {
             (component as IDamageable).TakeDamage(_damage);
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
     }
 }
