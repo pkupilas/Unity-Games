d03d8e710b9b7e001273088aaadde27938429333
pkupilas <pm.kupilas@gmail.com>
2017-07-21 13:16:43 +0200

    [Shrubbery] Added sawmill.

---
+++ Shrubbery/Assets/Scripts/Sawmill.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Sawmill : MonoBehaviour
+{
+
+    [SerializeField] private float _range = 10f;
+    private GameObject _target;
+
+
+	void Update ()
+	{
+	    CheckForTargets();
+	}
+    
+    void OnDrawGizmos()
+    {
+        Color color = Color.black;
+        color.a = 0.5f;
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere(transform.position, _range);
+    }
+
+    void CheckForTargets()
+    {
+        var colliderList = Physics.OverlapSphere(transform.position, _range, 1 << LayerMask.NameToLayer("Tree")).ToList();
+
+        if (_target == null || !colliderList.Contains(_target.GetComponent<Collider>()))
+        {
+            _target = colliderList.Count > 0 ? colliderList[0].gameObject : null;
+            print("iamgere");
+        }
+    }
+
+}
