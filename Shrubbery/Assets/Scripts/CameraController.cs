70b6e4e848769e06a92af3a5328438dc35e07ca8
pkupilas <pm.kupilas@gmail.com>
2017-07-24 11:50:59 +0200

    [Shrubbery] Added panning and zooming to camera.

---
+++ Shrubbery/Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+    private const float DistanceFromScreenEdge = 10f;
+    private const float MovingSpeed = 20f;
+    private const float ZoomingSpeed = 1000f;
+    private Camera _camera;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    void Update ()
+    {
+        float zoom = Input.GetAxis("Mouse ScrollWheel");
+        var pan = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - DistanceFromScreenEdge)
+        {
+            pan += new Vector3(MovingSpeed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= DistanceFromScreenEdge)
+        {
+            pan -= new Vector3(MovingSpeed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - DistanceFromScreenEdge)
+        {
+            pan += new Vector3(0f, MovingSpeed * Time.deltaTime, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= DistanceFromScreenEdge)
+        {
+            pan -= new Vector3(0f, MovingSpeed * Time.deltaTime, 0f);
+        }
+
+        _camera.orthographicSize += zoom * ZoomingSpeed * Time.deltaTime;
+
+        // TODO: Make clamping
+        gameObject.transform.Translate(pan);
+    }
+}
