6b7dfde747a6b72d808a461bd7fac4400498a2a3
pkupilas <pm.kupilas@gmail.com>
2017-07-25 13:54:50 +0200

    [DragonRPG] Fixed projectiles collision detection and destroying.

--- DragonRPG/Assets/Scripts/Projectile.cs
+++ DragonRPG/Assets/Scripts/Projectile.cs
@@ -6,12 +6,13 @@ public class Projectile : MonoBehaviour
     public float velocity;
     public float damage;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
         var component = other.gameObject.GetComponent(typeof(IDamageable));
         if (component)
         {
             (component as IDamageable).TakeDamage(damage);
         }
+        Destroy(gameObject);
     }
 }
