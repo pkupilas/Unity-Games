8e2bd4998a79d574be97137e89264bef20653798
pkupilas <pm.kupilas@gmail.com>
2017-08-09 11:30:04 +0200

    [DragonRPG] Added energy bar.

---
+++ DragonRPG/Assets/_Characters/Energy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour
+{
+    [SerializeField] private RawImage _energyBar;
+    [SerializeField] private float _maxEnergy;
+
+    private float _currentEnergy;
+
+	void Start ()
+	{
+	    _currentEnergy = _maxEnergy;
+	}
+}
