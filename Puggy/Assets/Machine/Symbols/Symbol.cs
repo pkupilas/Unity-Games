15e7fe2f57bb26bd77030ae8e888d91294a87354
pkupilas <pm.kupilas@gmail.com>
2017-08-11 12:40:50 +0200

    [Puggy] Added symbols logic.

---
+++ Puggy/Assets/Machine/Symbols/Symbol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Machine.Symbols
+{
+    public class Symbol : MonoBehaviour
+    {
+        [SerializeField] private List<SymbolData> _possibleDatas;
+        private SymbolData _symbolData;
+        private SpriteRenderer _spriteRenderer;
+
+        void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            GenerateSymbolData();
+            UpdateSprite();
+        }
+
+        private void GenerateSymbolData()
+        {
+            int randomIndex = Random.Range(0, _possibleDatas.Count);
+            Debug.Log(randomIndex);
+            _symbolData = _possibleDatas[randomIndex];
+        }
+
+        private void UpdateSprite()
+        {
+            _spriteRenderer.sprite = _symbolData.GetSprite();
+        }
+    }
+}
---
+++ Puggy/Assets/Machine/Symbols/SymbolData.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Machine.Symbols
+{
+    [CreateAssetMenu(menuName = "SymbolData")]
+    public class SymbolData : ScriptableObject
+    {
+        [SerializeField] private float _prize;
+        [SerializeField] private Sprite _sprite;
+
+        public float GetPrize()
+        {
+            return _prize;
+        }
+
+        public Sprite GetSprite()
+        {
+            return _sprite;
+        }
+
+        public void SetSprite(Sprite sprite)
+        {
+            _sprite = sprite;
+        }
+    }
+}
