71053ca17b7301eac638d1ee0237f71df7ee841f
pkupilas <pm.kupilas@gmail.com>
2017-08-11 19:42:17 +0200

    [Puggy] Added LevelManager and new scenes.

---
+++ Puggy/Assets/CameraUI/LevelManager/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CameraUI.LevelManager
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public void LoadLevel(string name)
+        {
+            SceneManager.LoadScene(name);
+        }
+
+        public void QuitRequest()
+        {
+            Application.Quit();
+        }
+
+        public void LoadNextLevel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    }
+}
