90e3f5fdc7b5e9212702e5daa8fa5c21d2d5331e
pkupilas <pm.kupilas@gmail.com>
2017-08-29 14:52:51 +0200

    [Aliens] Camera follow, temp assets, reference images added.

---
+++ Aliens/Assets/Camera & UI/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Player _player;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _player = FindObjectOfType<Player>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+	    transform.position = _player.transform.position;
+	}
+}
