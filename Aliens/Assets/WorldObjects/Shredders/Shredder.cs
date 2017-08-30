7e6ced136c79fe9481cf4b561f23949b4d946a68
pkupilas <pm.kupilas@gmail.com>
2017-08-30 13:01:53 +0200

    [Aliens] Added shredders.

---
+++ Aliens/Assets/WorldObjects/Shredders/Shredder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Weapons.Ammunition;
+
+namespace WorldObjects.Shredders
+{
+    public class Shredder : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision other)
+        {
+            var bulletComponent = other.gameObject.GetComponent<Bullet>();
+            if (bulletComponent)
+            {
+                Destroy(bulletComponent.gameObject);
+            }
+        }
+    }
+}
