f0466d5167a518d49a3a7447ede561ba3d0f63ed
pkupilas <pm.kupilas@gmail.com>
2017-08-31 21:05:07 +0200

    [Aliens] Added UI images for weapons and scroll wheel changing.

---
+++ Aliens/Assets/Camera & UI/ChangeWeaponUI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+using System.Linq;
+
+public class ChangeWeaponUI : MonoBehaviour
+{
+    private int _currentWeaponIndex = 0;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        InitializeUIWeaponImages();
+    }
+
+    private void InitializeUIWeaponImages()
+    {
+        ColorWeaponImages();
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        HandleScrollWheel();
+        ColorWeaponImages();
+    }
+
+    private void ColorWeaponImages()
+    {
+        foreach (Transform weaponImageTransform in transform)
+        {
+            weaponImageTransform.GetComponent<Image>().color = Color.black;
+
+        }
+        transform.GetChild(_currentWeaponIndex).GetComponent<Image>().color = Color.red;
+    }
+
+    private void HandleScrollWheel()
+    {
+        var scrollWheel = CrossPlatformInputManager.GetAxis("Mouse ScrollWheel");
+        if (scrollWheel==0) return;
+        
+        _currentWeaponIndex = scrollWheel > 0 ? _currentWeaponIndex + 1 : _currentWeaponIndex - 1;
+        _currentWeaponIndex = _currentWeaponIndex >= transform.childCount ? 0 : _currentWeaponIndex;
+        _currentWeaponIndex = _currentWeaponIndex < 0 ? transform.childCount-1 : _currentWeaponIndex;
+    }
+}
