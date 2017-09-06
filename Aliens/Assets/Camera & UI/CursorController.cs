72cb45172e5f8b7aab9d0167dba4b80abd54f27b
pkupilas <pm.kupilas@gmail.com>
2017-09-06 19:06:36 +0200

    [Aliens] Added cursor.

---
+++ Aliens/Assets/Camera & UI/CursorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    [SerializeField] private Texture2D _cursorTexture;
+    [SerializeField] private Vector2 _hotspot = new Vector2(0, 0);
+
+    void Start ()
+    {
+	    Cursor.SetCursor(_cursorTexture, _hotspot, CursorMode.Auto);	
+	}
+}
