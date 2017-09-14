06e077b9eb7d7781973916635942d074e6c402e6
pkupilas <pm.kupilas@gmail.com>
2017-09-14 14:55:07 +0200

    [DragonRPG] Added asteroids and adjusted ship.

---
+++ AsteroidShooter/Assets/WorldObjects/Asteroids/AsteroidData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace WorldObjects.Asteroids
+{
+    [CreateAssetMenu(menuName = "Asteroid")]
+    public class AsteroidData : ScriptableObject
+    {
+        [SerializeField] private GameObject _asteroidPrefab;
+
+        public GameObject AsteroidPrefab => _asteroidPrefab;
+    }
+}
---
+++ AsteroidShooter/Assets/WorldObjects/Asteroids/AsteroidSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WorldObjects.Asteroids;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    [SerializeField] private AsteroidData _asteroidData;
+    private float initialPositionX = -50.5f;
+    private float initialPositionY = 50.5f;
+
+    void Start()
+    {
+        SpawnAsteroids();
+    }
+
+    private void SpawnAsteroids()
+    {
+        for (float i = initialPositionX; i <= 49.5f; i+=1)
+        {
+            for (float j = initialPositionY; j >= -49.5; j-=1)
+            {
+                var asteroidPrefab = _asteroidData.AsteroidPrefab;
+                Instantiate(asteroidPrefab,new Vector3(i,j,0f),Quaternion.identity);
+            }
+        }
+    }
+}
