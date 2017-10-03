e9f91fb8e0d1231f78e933736ccb4c255318923b
pkupilas <pm.kupilas@gmail.com>
2017-10-03 16:25:55 +0200

    [DragonRPG] Added simple AI behaviour tree.

--- DragonRPG/Assets/_Characters/Enemies/EnemyAI.cs
+++ DragonRPG/Assets/_Characters/Enemies/EnemyAI.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
 using _Characters.CommonScripts;
 using _Characters.Player;
 
@@ -8,17 +10,49 @@ namespace _Characters.Enemies
     {
         [SerializeField] private float _chaseRadius = 10f;
         private float _currentWeaponRange;
-        private PlayerMovement _playerMovement;
-        
+        private PlayerMovement _player;
+        private Character _character;
+        private float _distanceToPlayer;
+        private enum Status { Idle, Attack, Patrol, Chase}
+
+        private Status _state = Status.Idle;
+
         void Start()
         {
-            _playerMovement = FindObjectOfType<PlayerMovement>();
+            _player = FindObjectOfType<PlayerMovement>();
+            _character = GetComponent<Character>();
         }
 
         void Update()
         {
-            var distanceToPlayer = Vector3.Distance(_playerMovement.transform.position, gameObject.transform.position);
+            _distanceToPlayer = Vector3.Distance(_player.transform.position, gameObject.transform.position);
             _currentWeaponRange = GetComponent<WeaponSystem>().CurrentWeaponConfig.MaxAttackRange;
+
+            if (_distanceToPlayer > _chaseRadius && _state != Status.Patrol)
+            {
+                StopAllCoroutines();
+                _state = Status.Patrol;
+            }
+            if (_distanceToPlayer <= _chaseRadius && _state != Status.Chase)
+            {
+                StopAllCoroutines();
+                _state = Status.Chase;
+                StartCoroutine(ChasePlayer());
+            }
+            if (_distanceToPlayer <= _currentWeaponRange && _state != Status.Attack)
+            {
+                StopAllCoroutines();
+                _state = Status.Attack;
+            }
+        }
+
+        private IEnumerator ChasePlayer()
+        {
+            while (_distanceToPlayer >= _currentWeaponRange)
+            {
+                _character.SetDestination(_player.transform.position);
+                yield return new WaitForEndOfFrame();
+            }
         }
 
         private void OnDrawGizmos()
