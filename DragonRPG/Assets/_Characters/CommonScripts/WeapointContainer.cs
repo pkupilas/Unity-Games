1de3b0f6553575d94c57e5d95ee753f74a57a08a
pkupilas <pm.kupilas@gmail.com>
2017-10-04 10:56:07 +0200

    [DragonRPG] Added partol paths concept.

---
+++ DragonRPG/Assets/_Characters/CommonScripts/WeapointContainer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace _Characters.CommonScripts
+{
+    public class WeapointContainer : MonoBehaviour
+    {
+        void OnDrawGizmos()
+        {
+            foreach (Transform childTransform in transform)
+            {
+                Gizmos.color = new Color(255f, 0f, 0f, 0.5f);
+                Gizmos.DrawSphere(childTransform.position, 0.5f);
+            }
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Gizmos.color = new Color(0f, 0f, 255f, 0.5f);
+                Gizmos.DrawLine(transform.GetChild(i).position,
+                    i == transform.childCount - 1 ? transform.GetChild(0).position : transform.GetChild(i + 1).position);
+            }
+        }
+    }
+}
