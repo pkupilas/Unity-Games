8d0c4000beebac75ede36cfef6205ce250b26c9f
pkupilas <pm.kupilas@gmail.com>
2018-02-25 22:04:50 +0100

    [Shrubbery] Added changing camera controll input. Added shredders for projectiles.

--- Shrubbery/Assets/Scripts/CameraController.cs
+++ Shrubbery/Assets/Scripts/CameraController.cs
@@ -1,44 +1,33 @@
-﻿using UnityEngine;
-
+﻿using System;
+using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public enum CameraInput
+    {
+        Mouse,
+        Keyboard,
+        MouseAndKeyboard,
+        Invalid
+    }
+
     private const float DistanceFromScreenEdge = 10f;
     private const float MovingSpeed = 20f;
     private const float ZoomingSpeed = 1000f;
     private Vector3 _initialCameraPosition = Vector3.zero;
     private Camera _camera;
+    public CameraInput cameraInput = CameraInput.Keyboard;
 
-    void Start()
+    private void Start()
     {
         _camera = GetComponent<Camera>();
         _initialCameraPosition = gameObject.transform.position;
     }
 
-    void Update ()
+    private void Update()
     {
         float zoom = Input.GetAxis("Mouse ScrollWheel");
-        var pan = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - DistanceFromScreenEdge)
-        {
-            pan += new Vector3(MovingSpeed * Time.deltaTime, 0f, 0f);
-        }
-
-        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= DistanceFromScreenEdge)
-        {
-            pan -= new Vector3(MovingSpeed * Time.deltaTime, 0f, 0f);
-        }
-
-        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - DistanceFromScreenEdge)
-        {
-            pan += new Vector3(0f, MovingSpeed * Time.deltaTime, 0f);
-        }
-
-        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= DistanceFromScreenEdge)
-        {
-            pan -= new Vector3(0f, MovingSpeed * Time.deltaTime, 0f);
-        }
+        var pan = GetCameraPositionBy();
 
         _camera.orthographicSize += zoom * ZoomingSpeed * Time.deltaTime;
 
@@ -51,4 +40,89 @@ public class CameraController : MonoBehaviour
             gameObject.transform.position = _initialCameraPosition;
         }
     }
+
+    private Vector3 GetCameraPositionBy()
+    {
+        var pan = Vector3.zero;
+
+        switch (cameraInput)
+        {
+            case CameraInput.Invalid:
+                return Vector3.zero;
+            case CameraInput.Keyboard:
+                pan = HandleByKeyboard();
+                break;
+            case CameraInput.Mouse:
+                pan = HandleByMouse();
+                break;
+            //case CameraInput.MouseAndKeyboard:
+            //    pan = HandleByMouseAndKeyboard();
+            //    break;
+            default:
+                Debug.Log($"Camera input state not handled {cameraInput}");
+                break;
+        }
+
+
+        return pan;
+    }
+
+    private static Vector3 HandleByKeyboard()
+    {
+        var pan = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            pan += new Vector3(MovingSpeed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            pan -= new Vector3(MovingSpeed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            pan += new Vector3(0f, MovingSpeed * Time.deltaTime, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            pan -= new Vector3(0f, MovingSpeed * Time.deltaTime, 0f);
+        }
+
+        return pan;
+    }
+
+    private static Vector3 HandleByMouse()
+    {
+        var pan = Vector3.zero;
+
+        if (Input.mousePosition.x >= Screen.width - DistanceFromScreenEdge)
+        {
+            pan += new Vector3(MovingSpeed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.mousePosition.x <= DistanceFromScreenEdge)
+        {
+            pan -= new Vector3(MovingSpeed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.mousePosition.y >= Screen.height - DistanceFromScreenEdge)
+        {
+            pan += new Vector3(0f, MovingSpeed * Time.deltaTime, 0f);
+        }
+
+        if (Input.mousePosition.y <= DistanceFromScreenEdge)
+        {
+            pan -= new Vector3(0f, MovingSpeed * Time.deltaTime, 0f);
+        }
+
+        return pan;
+    }
+
+    //private void HandleByMouseAndKeyboard()
+    //{
+    //    return HandleByMouse(HandleByKeyboard());
+    //}
 }
---
+++ Shrubbery/Assets/Scripts/Shredder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var component = other.gameObject.GetComponent<Projectile>();
+        if (component)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
