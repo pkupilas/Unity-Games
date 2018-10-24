428235e5ac43f058c9f77e1e915516f6dc072712
pkupilas <pm.kupilas@gmail.com>
2018-10-24 22:01:34 +0200

    [TileVania] Enabled run animation while moving.

--- TileVania/Assets/Scripts/Unity/Views/PlayerView.cs
+++ TileVania/Assets/Scripts/Unity/Views/PlayerView.cs
@@ -1,20 +1,28 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerView : MonoBehaviour
 {
     [SerializeField]
     private MovementConfiguration _MovementConfiguration;
     private Rigidbody2D _Rigidbody;
+    private Animator _Animator;
     private float _HorizontalInput = 0.0f;
     private bool _ShouldJump = false;
+
+    private const string _RUN_PARAMETER_NAME = "IsRunning";
 
     private void Awake()
     {
         _Rigidbody = GetComponent<Rigidbody2D>();
+        _Animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        SetInput();
+        SetAnimatorParameters();
         Move();
         FlipCharacter();
     }
@@ -27,17 +35,30 @@ public class PlayerView : MonoBehaviour
         }
     }
 
-    private void Move()
+    private void SetInput()
     {
         _HorizontalInput = hInput.GetAxis("Horizontal");
         _ShouldJump = hInput.GetButtonDown("Jump");
+    }
 
+    private void SetAnimatorParameters()
+    {
+        _Animator.SetBool(_RUN_PARAMETER_NAME, IsPlayerRunning());
+    }
+
+    private bool IsPlayerRunning()
+    {
+        return Mathf.Abs(_HorizontalInput) > Mathf.Epsilon;
+    }
+
+    private void Move()
+    {
         transform.position += Vector3.right * _HorizontalInput * Time.deltaTime * _MovementConfiguration.MovementSpeed;
     }
 
     private void FlipCharacter()
     {
-        if (Mathf.Abs(_HorizontalInput) < Mathf.Epsilon)
+        if (!IsPlayerRunning())
             return;
 
         transform.localScale = new Vector3(
