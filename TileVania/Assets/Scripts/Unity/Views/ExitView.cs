31cd3dbb37d35f7dbd4412dce8db722b54624f77
pkupilas <pm.kupilas@gmail.com>
2018-11-19 21:38:34 +0100

    [TileVania] Added exit from level.

---
+++ TileVania/Assets/Scripts/Unity/Views/ExitView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitView : MonoBehaviour
+{
+    [SerializeField]
+    private string _NextLevelSceneName = string.Empty;
+
+    [SerializeField]
+    private float _LoadDelay = 2.0f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag((Utilities.Constans.Tag.PLAYER)))
+        {
+            StartCoroutine(LoadNextLevelWithDelay());
+        }
+    }
+
+    private IEnumerator LoadNextLevelWithDelay()
+    {
+        yield return new WaitForSeconds(_LoadDelay);
+
+        SceneManager.LoadSceneAsync(_NextLevelSceneName);
+    } 
+}
