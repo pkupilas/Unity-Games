1bb1c36c904e423c7efea608e6314d815fe40e2f
pkupilas <pm.kupilas@gmail.com>
2018-11-20 19:46:24 +0100

    [TileVania] Added start and end screen.

---
+++ TileVania/Assets/Scripts/Unity/Other/Menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    private string _NextLevelSceneName = string.Empty;
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadSceneAsync(_NextLevelSceneName);
+    }
+}
